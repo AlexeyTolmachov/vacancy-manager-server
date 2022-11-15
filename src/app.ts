29452bdb6b9eeebe2bec2c676477884f6d90216f
Aleksandr Podmazko <87004285+Aleksandr-Pod@users.noreply.github.com>
2022-11-15 23:45:09 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("The !");
+  res.send("The Mother fucker !!!");
 });
 
 app.listen(port, () => console.log(`server is listening on ${port}`));
